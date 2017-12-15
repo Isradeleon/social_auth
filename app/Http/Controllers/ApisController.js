7ab71f91d6b02a9566cac24d304032ffd6a1e3ee
= <=>
2017-12-15 12:41:08 -0600

    Displaying user info

--- app/Http/Controllers/ApisController.js
+++ app/Http/Controllers/ApisController.js
@@ -19,33 +19,35 @@ class ApisController {
 	// API Callbacks
 	* handle_instagram (request, response) {
 		const user = yield request.ally.driver('instagram').getUser()
-		return user.getNickname()
+		
+		return yield sendView('users.view',{
+			email: user.getEmail(),
+			avatar: user.getAvatar(),
+			username: user.getName(),
+			nickname: user.getNickname()
+	    })
 	}
 
 	* handle_twitter (request, response) {
 		const user = yield request.ally.driver('twitter').getUser()
-		return user.getNickname()
+		
+		return yield sendView('users.view',{
+			email: user.getEmail(),
+			avatar: user.getAvatar(),
+			username: user.getName(),
+			nickname: user.getNickname()
+	    })
 	}
 
 	* handle_facebook (request, response) {
 		const user = yield request.ally.driver('facebook').getUser()
-		return user.getNickname()
-	 //    const searchAttr = {
-	 //      email: fbUser.getEmail()
-	 //    }
-
-	 //    const newUser = {
-	 //      email: fbUser.getEmail(),
-	 //      avatar: fbUser.getAvatar(),
-	 //      username: fbUser.getName()
-	 //    }
-
-	// console.log(request.ally.driver('facebook'))
-	// res.send('ok')
-
-	    // const user = yield User.findOrCreate(searchAttr, newUser)
-
-	    // request.auth.loginViaId(user.id)
+		
+		return yield sendView('users.view',{
+			email: user.getEmail(),
+			avatar: user.getAvatar(),
+			username: user.getName(),
+			nickname: user.getNickname()
+	    })
 	}
 }
 
